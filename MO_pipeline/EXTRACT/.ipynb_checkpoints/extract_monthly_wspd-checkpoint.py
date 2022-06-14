db3bc6e94a679cef8bddaf11dc1878f6fd73b4ca
Tereza Jarnikova <tjarnik@gmail.com>
2022-06-14 01:52:45 +0100

    windroses

---
+++ MO_pipeline/EXTRACT/.ipynb_checkpoints/extract_monthly_wspd-checkpoint.py
@@ -0,0 +1,106 @@
+import glob
+import pickle
+import netCDF4 as nc
+import numpy as np
+ 
+runhorse = True
+rdir = '/gpfs/home/mep22dku/scratch/MET_soft/'
+dir_1H = 'hist/u-bc370_hist/'
+dir_2H = 'hist/u-cj198_hist_1950start1950ozone/'
+dir_3H = 'hist/u-cj200_hist_1990start1990ozone/'
+
+dir_1FA = 'ssp126/u-be682_ssp126/'
+dir_1FB = 'ssp370/u-ce417_ssp370/'
+
+dir_2FA = 'ssp126/u-cj880_ssp126_1950start1950ozone/'
+dir_2FB = 'ssp370/u-cj881_ssp370_1950start1950ozone/'
+
+dir_3FA = 'ssp126/u-cj484_ssp126_1990start1990ozone/'
+dir_3FB = 'ssp370/u-cj504_ssp370_1990start1990ozone/'
+
+def return_mean_SO_wspd(tdir,yr):
+    
+    days_in_month = np.array([31,28,31,30,31,30,31,31,30,31,30,31])
+    starts = np.zeros_like(days_in_month)
+    ends = np.zeros_like(starts)
+
+    monthly_sts = np.zeros([12,3])
+    monthly_sts[:] = np.nan
+    
+    for i in range(0,12):
+        starts[i] = np.sum(days_in_month[0:i])
+        ends[i] = np.sum(days_in_month[0:i+1])
+    
+    
+    rdir = f'/gpfs/home/mep22dku/scratch/MET_soft/{tdir}'
+    uf_f = glob.glob(f'{rdir}/taux_1d_{yr}_daily.nc')
+    vf_f = glob.glob(f'{rdir}/tauy_1d_{yr}_daily.nc')
+
+    try:
+        uf = nc.Dataset(uf_f[0])
+        uf_nc = (uf['uflx'])
+        vf = nc.Dataset(vf_f[0])
+        vf_nc = (vf['vflx'])
+        
+        #south of 50
+        for i in range(0,12):
+            uf_t_mon = uf_nc[starts[i]:ends[i],0:37,:]
+            vf_t_mon = uf_nc[starts[i]:ends[i],0:37,:]
+            t_mon = np.sqrt(uf_t_mon**2 + vf_t_mon**2)
+            monthly_sts[i,0] = np.nanmean(t_mon)
+            monthly_sts[i,1] = np.nanmax(t_mon)
+            monthly_sts[i,2] = np.nanmin(t_mon)
+
+    except:
+        x = 'we'
+                    
+    return monthly_sts
+    
+def calc_stats_wspd(tdir, yrstart, yrend):
+    yrs = np.arange(yrstart,yrend,1)
+    stor = np.zeros([len(yrs),12,3])
+    for i in range(0,len(yrs)):
+        yr = yrs[i]
+        if yr%50 == 0: print(yr)
+        monthly_sts = return_mean_SO_wspd(tdir,yr)
+        stor[i,:,:] = monthly_sts
+    
+    return stor
+
+
+if runhorse:
+    tnam = 'scen_1H'; tdir = dir_1H; yrstart = 1940; yrend = 2015
+    stor = calc_stats_wspd(tdir,yrstart,yrend)
+    pickle.dump(stor, open(f"./pkls/{tnam}_wspd_{yrstart}_{yrend}.pkl", 'wb'))
+
+    tnam = 'scen_2H'; tdir = dir_2H; yrstart = 1950; yrend = 2015
+    stor = calc_stats_wspd(tdir,yrstart,yrend)
+    pickle.dump(stor, open(f"./pkls/{tnam}_wspd_{yrstart}_{yrend}.pkl", 'wb'))
+
+    tnam = 'scen_3H'; tdir = dir_3H; yrstart = 1990; yrend = 2015
+    stor = calc_stats_wspd(tdir,yrstart,yrend)
+    pickle.dump(stor, open(f"./pkls/{tnam}_wspd_{yrstart}_{yrend}.pkl", 'wb'))
+
+    tnam = 'scen_1FA'; tdir = dir_1FA; yrstart = 2015; yrend = 2101
+    stor = calc_stats_wspd(tdir,yrstart,yrend)
+    pickle.dump(stor, open(f"./pkls/{tnam}_wspd_{yrstart}_{yrend}.pkl", 'wb'))
+
+    tnam = 'scen_2FA'; tdir = dir_2FA; yrstart = 2015; yrend = 2101
+    stor = calc_stats_wspd(tdir,yrstart,yrend)
+    pickle.dump(stor, open(f"./pkls/{tnam}_wspd_{yrstart}_{yrend}.pkl", 'wb'))
+
+    tnam = 'scen_3FA'; tdir = dir_3FA; yrstart = 2015; yrend = 2101
+    stor = calc_stats_wspd(tdir,yrstart,yrend)
+    pickle.dump(stor, open(f"./pkls/{tnam}_wspd_{yrstart}_{yrend}.pkl", 'wb'))
+
+    tnam = 'scen_1FB'; tdir = dir_1FB; yrstart = 2015; yrend = 2101
+    stor = calc_stats_wspd(tdir,yrstart,yrend)
+    pickle.dump(stor, open(f"./pkls/{tnam}_wspd_{yrstart}_{yrend}.pkl", 'wb'))
+
+    tnam = 'scen_2FB'; tdir = dir_2FB; yrstart = 2015; yrend = 2101
+    stor = calc_stats_wspd(tdir,yrstart,yrend)
+    pickle.dump(stor, open(f"./pkls/{tnam}_wspd_{yrstart}_{yrend}.pkl", 'wb'))
+
+    tnam = 'scen_3FB'; tdir = dir_3FB; yrstart = 2015; yrend = 2101
+    stor = calc_stats_wspd(tdir,yrstart,yrend)
+    pickle.dump(stor, open(f"./pkls/{tnam}_wspd_{yrstart}_{yrend}.pkl", 'wb'))
---
+++ MO_pipeline/EXTRACT/.ipynb_checkpoints/monthly_uv_vel_era_grid-checkpoint.py
@@ -0,0 +1,97 @@
+import netCDF4 as nc
+import xarray as xr
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+import sys
+sys.path.append('..')
+import scendict as sc
+
+starts = np.array([0, 31,  59,  90, 120, 151, 181, 212, 243, 273, 304, 334])
+ends = np.array([31,  59,  90, 120, 151, 181, 212, 243, 273, 304, 334, 365])
+
+scens = ['ERA5']
+
+for s in range(0,1):
+    scen = scens[s]
+    t_stor_u = np.random.rand(161,15,149,182)
+    t_stor_v = np.random.rand(161,15,149,182)
+    ncnam = f'./ncs/{scen}_monthly_u_v_vel.nc'
+    print(ncnam)
+    for yr in range(1940,2101):
+        if yr%10 == 0:
+            print(yr)
+        try:
+            tdir = '/gpfs/data/greenocean/software/products/ERA5Forcing/daily/'
+            tfil = f'tauy_1d_{yr}_daily.nc'
+            MO = nc.Dataset(f'{tdir}{tfil}')
+            t_nc_v = MO['vflx'][:]
+
+            tfil = f'taux_1d_{yr}_daily.nc'
+            MO = nc.Dataset(f'{tdir}{tfil}')
+            t_nc_u = MO['uflx'][:]
+            
+            # tw = nc.Dataset(f'{rdir}scen_{scen}/MetOffice_tauy_y{yr}.nc') 
+            # t_nc_v = tw['vflx'][:]
+            # tw = nc.Dataset(f'{rdir}scen_{scen}/MetOffice_taux_y{yr}.nc') 
+            # t_nc_u = tw['uflx'][:]
+            #t_nc = np.sqrt(t_nc1**2+t_nc2**2)
+
+            for i in range(0,12):
+                t_mon = t_nc_u[starts[i]:ends[i],:,:]
+                t_avg = np.nanmean(t_mon, axis = 0)
+                t_stor_u[yr-1940,i,:,:] = t_avg
+
+            summer = np.concatenate((t_nc_u[0:59,:,:],t_nc_u[334:365,:,:]),axis = 0)
+            t_stor_u[yr-1940,12,:,:] = np.nanmean(summer,axis =0)
+            winter = (t_nc_u[151:243,:,:])
+            t_stor_u[yr-1940,13,:,:] = np.nanmean(winter,axis = 0)
+            t_stor_u[yr-1940,14,:,:] = np.nanmean(t_nc_u,axis = 0)
+            
+            for i in range(0,12):
+                t_mon = t_nc_v[starts[i]:ends[i],:,:]
+                t_avg = np.nanmean(t_mon, axis = 0)
+                t_stor_v[yr-1940,i,:,:] = t_avg
+
+            summer = np.concatenate((t_nc_v[0:59,:,:],t_nc_v[334:365,:,:]),axis = 0)
+            t_stor_v[yr-1940,12,:,:] = np.nanmean(summer,axis =0)
+            winter = (t_nc_v[151:243,:,:])
+            t_stor_v[yr-1940,13,:,:] = np.nanmean(winter,axis = 0)
+            t_stor_v[yr-1940,14,:,:] = np.nanmean(t_nc_v,axis = 0)
+            
+        except:
+            t_stor_u[yr-1940,:,:,:] = np.nan
+            t_stor_v[yr-1940,:,:,:] = np.nan
+
+
+    ds = xr.Dataset(
+        {
+            "mean_uvel": (("yr", "mon", "y", "x" ), t_stor_u),
+            "mean_vvel": (("yr", "mon", "y", "x" ), t_stor_v)
+        },
+        coords={
+            "yr": np.arange(1940,2101,1),
+            "mon": np.arange(1,16,1),
+            "x": np.arange(0,182,1),
+            "y": np.arange(0,149,1),
+        },
+        attrs={
+            "desc": "mean monthly maps of u and v vel, made from ~/scratch/MET_forcing/",
+            "desc2": "summer is month 13, winter is month 14, full year is month 15 (index 14)"
+      }
+       )
+    
+#     In [35]: ds = xr.Dataset(
+#    ....:     {
+#    ....:         "temperature": (["x", "y", "time"], temp),
+#    ....:         "precipitation": (["x", "y", "time"], precip),
+#    ....:     },
+#    ....:     coords={
+#    ....:         "lon": (["x", "y"], lon),
+#    ....:         "lat": (["x", "y"], lat),
+#    ....:         "time": pd.date_range("2014-09-06", periods=3),
+#    ....:         "reference_time": pd.Timestamp("2014-09-05"),
+#    ....:     },
+#    ....: )
+        
+    ds.to_netcdf(ncnam)
